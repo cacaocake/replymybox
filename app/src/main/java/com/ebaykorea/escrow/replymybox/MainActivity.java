77a58f4e687592df8d81f07627871eccf84a647b
강경구 <cacao@gang-gyeong-guui-MacBook-Air.local>
2016-02-05 01:57:38 +0900

    Scan 버튼으로 하게끔

--- app/src/main/java/com/ebaykorea/escrow/replymybox/MainActivity.java
+++ app/src/main/java/com/ebaykorea/escrow/replymybox/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ebaykorea.escrow.replymybox.service.LocationService;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -17,6 +20,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button scan = (Button)findViewById(R.id.barcode_scan_button);
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanBarcode();
+            }
+        });
+
+    }
+    public void scanBarcode() {
+        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+        integrator.initiateScan();
     }
 
     @Override
@@ -35,11 +52,11 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            //Intent serviceIntent = new Intent(this, LocationService.class);
-            //this.startService(serviceIntent);
+//            Intent serviceIntent = new Intent(this, LocationService.class);
+//            this.startService(serviceIntent);
 
-            IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-            integrator.initiateScan();
+//            IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+//            integrator.initiateScan();
 
             return true;
         }
@@ -50,8 +67,14 @@ public class MainActivity extends ActionBarActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanResult != null) {
-            String re = scanResult.getContents();
-            Log.d("code", re);
+            if(scanResult.getContents() != null) {
+                String re = scanResult.getContents();
+                Log.d("code", re);
+
+                TextView barcodeTextView = (TextView)findViewById(R.id.barcode_textview);
+                barcodeTextView.setText(re);
+            }
+
         }
         // else continue with any other code you need in the method
 
